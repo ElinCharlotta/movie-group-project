aae1de273c14fa46ec4d74d6fa193303b5e594f3
JassinAlSafe <121439769+JassinAlSafe@users.noreply.github.com>
2024-09-30 13:58:01 +0200

    we made a basic setup for main to see the fetched data from movies

--- movie-project/src/Pages/Home.tsx
+++ movie-project/src/Pages/Home.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-function Home() {
+interface MovieProps {
+  title: string
+  thumbnail: string
+}
+
+const Home: React.FC = () => {
+  const [movies, setMovies] = useState<MovieProps[]>([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    import('../../public/movies.json').then(res => {
+      setMovies(res.default)
+      setLoading(false)
+    })
+  }, []) // Add dependency array to run only once
+
+  if (loading) return <div>Loading...</div>
+
   return (
-    <div>Home</div>
+    <div>
+      {movies.map((movie, index) => (
+        <div key={index}>
+          <h1>{movie.title}</h1>
+          <img src={movie.thumbnail} alt={movie.title} />
+        </div>
+      ))}
+    </div>
   )
 }
 
