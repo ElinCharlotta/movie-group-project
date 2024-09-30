6a0bc4b75a7dbfe37240f6c5eff5e4e86719eca9
ElinCharlotta <janssonelin@hotmail.com>
2024-09-30 15:40:10 +0200

    added base for porject

--- movie-project/vite.config.ts
+++ movie-project/vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "/movie-group-project/",
   plugins: [react()],
   test: {
     globals: true,
