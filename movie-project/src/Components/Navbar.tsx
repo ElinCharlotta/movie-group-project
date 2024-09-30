ecffcf4db3755960265d4194690cd1b8766090ff
JassinAlSafe <121439769+JassinAlSafe@users.noreply.github.com>
2024-09-30 14:28:50 +0200

    updated the style of the navbar and added a searchbar aswell as implemented lucide react for the icons used in the navbar

--- movie-project/src/Components/Navbar.tsx
+++ movie-project/src/Components/Navbar.tsx
@@ -1,18 +1,25 @@
 import { Link } from 'react-router-dom'
-import { Search } from 'lucide-react'
+import { Search, Projector } from 'lucide-react'
 import './Styles/Navbar.css'
 
 const Navbar: React.FC = () => {
   return (
     <nav className='navbar'>
       <div className='navbar-header'>
-        <h1>app</h1>
+        <Projector color='white' size={40} />
       </div>
-      <ul>
+  
+      <ul className='navbar-links'>
         <Link to='/'>Home</Link>
         <Link to='/bookmarked'>Bookmarked</Link>
         <Link to='/categories'>Categories</Link>
       </ul>
+      <div className='navbar-search'>
+            <input type="text" placeholder='Search Movies...' />
+            <button aria-label='Search'>
+                <Search className='h-4 w-4'/>
+            </button>
+      </div>
     </nav>
   )
 }
