7d848d9e751c0c86001778d812a01476e065ab49
JassinAlSafe <121439769+JassinAlSafe@users.noreply.github.com>
2024-09-30 11:55:25 +0200

    update navbar

--- movie-project/src/Components/Navbar.tsx
+++ movie-project/src/Components/Navbar.tsx
@@ -1,14 +1,19 @@
 import { Link } from "react-router-dom";
+import "../Styles/Navbar.css";
 
 const Navbar: React.FC = () => {
-    return (
-        <>
-            <div className="navbar-header"></div>
-            <Link to="/">Home</Link>
-            <Link to="/bookmarked">Bookmarked</Link>
-            <Link to="/categories">Categories</Link>
-        </>
-    );
+  return (
+    <nav className="navbar">
+      <div className="navbar-header">
+        <h1>app</h1>
+      </div>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/bookmarked">Bookmarked</Link>
+        <Link to="/categories">Categories</Link>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
