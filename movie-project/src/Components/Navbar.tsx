6b479f838d8bd7216c7922be16ae5c5a8c3e3b95
JassinAlSafe <121439769+JassinAlSafe@users.noreply.github.com>
2024-09-30 14:58:32 +0200

    added a menu for mobile view so we get a dropdown menu when we are in a mobile view.

--- movie-project/src/Components/Navbar.tsx
+++ movie-project/src/Components/Navbar.tsx
@@ -1,25 +1,41 @@
 import { Link } from 'react-router-dom'
-import { Search, Projector } from 'lucide-react'
+import { Search, Projector, Menu } from 'lucide-react'
+import { useState } from 'react'
 import './Styles/Navbar.css'
 
 const Navbar: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
+  }
+
   return (
     <nav className='navbar'>
       <div className='navbar-header'>
         <Projector color='white' size={40} />
+        <button className='mobile-menu-toggle' onClick={toggleMenu}>
+          <Menu size={12} />
+        </button>
       </div>
-  
-      <ul className='navbar-links'>
-        <Link to='/'>Home</Link>
-        <Link to='/bookmarked'>Bookmarked</Link>
-        <Link to='/categories'>Categories</Link>
+
+      <ul className={`navbar-links ${isMenuOpen ? 'active' : ''}`}>
+        <li>
+          <Link to='/' onClick={() => setIsMenuOpen(false)}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to='/bookmarked' onClick={() => setIsMenuOpen(false)}>
+            Bookmarked
+          </Link>
+        </li>
+        <li>
+          <Link to='/categories' onClick={() => setIsMenuOpen(false)}>
+            Categories
+          </Link>
+        </li>
       </ul>
-      <div className='navbar-search'>
-            <input type="text" placeholder='Search Movies...' />
-            <button aria-label='Search'>
-                <Search className='h-4 w-4'/>
-            </button>
-      </div>
     </nav>
   )
 }
