24eea7d1b0b91553dc6c0dc77ec4b1d72fde426c
Terry vanker <Terry.vanker89@gmail.com>
2024-10-01 21:54:57 +0200

    added MovieCardComponent + implemented in Home to see it work

---
+++ movie-project/src/Components/MovieCard/MovieCard.tsx
@@ -0,0 +1,33 @@
+interface MovieCardProps {
+  title: string
+  year: number
+  rating?: string
+  actors?: string[]
+  genre?: string
+  thumbnail: string
+  index: number
+}
+
+const MovieCard: React.FC<MovieCardProps> = ({
+  title,
+  year,
+  genre,
+  thumbnail,
+  index,
+}) => {
+  return (
+    <div className='card-container'>
+      <h2>
+        {index + 1}. {title} ({year})
+      </h2>
+      <img src={thumbnail} alt={`${title} thumbnail`} />
+      <p>Genre: {genre}</p>
+    </div>
+  )
+}
+
+export default MovieCard
+
+//  Har med de sakerna ifrån "db.en" jag tänkte var användbart.
+// har tagit med index, ifall man vill göra en delete eller liknande längre fram
+//
