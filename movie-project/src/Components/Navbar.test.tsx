4992e508df9437907a4063d2d04e7371e016998f
JassinAlSafe <121439769+JassinAlSafe@users.noreply.github.com>
2024-10-02 16:17:59 +0200

    installed msw aswell as user event from testing-library

---
+++ movie-project/src/Components/Navbar.test.tsx
@@ -0,0 +1,5 @@
+import { render, screen } from '@testing-library/react'
+import { describe, it, expect, beforeAll, afterEach, afterAll, vi } from 'vitest'
+import { setupServer } from 'msw/node'
+import { http, HttpResponse } from 'msw'
+import userEvent from '@testing-library/user-event'
