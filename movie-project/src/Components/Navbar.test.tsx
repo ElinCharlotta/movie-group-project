2b3cc20d82298a3dca47154ec5826eebbc6ae651
JassinAlSafe <121439769+JassinAlSafe@users.noreply.github.com>
2024-10-03 00:35:22 +0200

    removed some unused file that we tested

--- movie-project/src/Components/Navbar.test.tsx
+++ movie-project/src/Components/Navbar.test.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import '@testing-library/jest-dom'
 import { describe, it, expect, vi } from 'vitest'
 import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 import Navbar from './Navbar'
 import { BrowserRouter as Router } from 'react-router-dom'
 
