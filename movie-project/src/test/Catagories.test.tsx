10f55c9d3e4365acc06b77a5c1824d69c5fdae48
Mohsin Rahman <mohsinrahmang@gmail.com>
2024-10-07 13:03:55 +0200

    Categories

---
+++ movie-project/src/test/Catagories.test.tsx
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react'
+import Categories from '../Pages/Categories.tsx'
+import { describe, it, expect,test } from 'vitest'
+import { userEvent } from '@testing-library/user-event'
+
+it('renders correctly',  () => {
+    const  comp = render(<Categories />);
+    expect(comp).toMatchSnapshot();  
+  })
+
+  it("Test for a word that does exist", async () => {
+    render(<Categories />);
+    const el = await screen.findByText("Movie Categories");
+    expect(el).toBeInTheDocument();
+  });
+
+  it("Test for a button  that display result", async () => {
+    render(<Categories />);
+    const el = await screen.findByText("Drama");
+    await userEvent.click(el)
+    const elp = await screen.findByText("One Flew Over the Cuckoo's Nest");
+    expect(elp).toBeInTheDocument(); 
+  });
