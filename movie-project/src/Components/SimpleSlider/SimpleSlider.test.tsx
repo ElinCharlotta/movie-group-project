4181513f9a00682ba41b0e4576bfe4bdeef94c93
ElinCharlotta <janssonelin@hotmail.com>
2024-10-08 16:44:07 +0200

    Update SimpleSlider test

--- movie-project/src/Components/SimpleSlider/SimpleSlider.test.tsx
+++ movie-project/src/Components/SimpleSlider/SimpleSlider.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, waitFor } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import SimpleSlider from './SimpleSlider'
 import { BrowserRouter } from 'react-router-dom'
